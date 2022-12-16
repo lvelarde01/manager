debfb60010045eb90637ea4561110bab92b66b43
Luis Velarde <velardeluisangel@gmail.com>
2022-12-16 09:45:36 -0400

    [Add] New component for messages.

--- src/assets/alertmessage.js
+++ src/assets/alertmessage.js
@@ -1,11 +1,33 @@
-import React from 'react'
-
-export default function AlertMessage({Elementshow=true}) {
-    if(!Elementshow)return (<></>);
-
+import React,{useState,useEffect} from 'react'
+const defaultConfig = {
+  success:{
+      message:'Se guardo exitosamente.',
+      classIcon:'fas fa-floppy-disk me-2',
+      sizeClass:'col-11 mt-3',   
+  },
+  danger:{
+    message:'Se presento un problema. Por favor, verifique e intente de nuevo.',
+    classIcon:'fas fa-triangle-exclamation me-2',
+    sizeClass:'col-11 mt-3',   
+  },
+  info:{
+    message:'Se informa que todo funciono correctamente.',
+    classIcon:'fas fa-circle-info me-2',
+    sizeClass:'col-11 mt-3',   
+  }
+}
+export default function AlertMessage({message='', msgtype='success',sizeClass='',classIcon=''}) {
+  const [show,setShow]=useState(true);
+  useEffect(()=>{
+    setTimeout(()=>{
+      console.log('alert hide');
+      setShow(false);
+    },3000)
+  },[]);  
+  
   return (
-    <div className='alert alert-success alert-dismissible col-11 mt-3'>
-            <i className='fas fa-floppy-disk me-2'></i>Se Actualizo Correctamente
+    <div className={`alert alert-${msgtype} alert-dismissible  ${sizeClass || defaultConfig?.[msgtype]?.sizeClass}  ${!show? 'collapse' : '' } `}>
+            <i className={classIcon || defaultConfig?.[msgtype]?.classIcon }></i>{defaultConfig?.[msgtype]?.message}
             <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
   )
