122151bd1dfcd41cd521dafbe3b8c2baadb1a85f
Luis Velarde <velardeluisangel@gmail.com>
2022-12-16 09:51:25 -0400

    [Add] New options for fetch and validation data.

--- src/requests/users.js
+++ src/requests/users.js
@@ -1,9 +1,179 @@
 import localforage from "localforage";
+const URLREMOTE = 'https://manager-backend.lvelarde01.repl.co';
+const URLDEV = 'http://127.0.0.1:3000';
+export const schema = {
+  username:{
+              rules:[
+                      {
+                        name:'onlyNumberAndLetter',
+                        message:'Solo permitido letras y numeros.'
+                      },
+                      {
+                        name:'isUnique',
+                        message:'Usuario ya Registrado.'
+                      },
+                    ]
+            },
+  password:{
+              rules:[
+                      {
+                        name:'onlyNumberAndLetterSimbols',
+                        message:'Numero y letras. Simbolos permitidos [@,.,-,_,#]'
+                      },
+                      {
+                        name:'compareField',
+                        matchField:'repeatpassword',
+                        message:'No Coincide la contrasena.'
+                      },
+                      {
+                        name:'minMaxLength',
+                        message:'Minimo de caracteres 5 y maximo 10.',
+                        minLength:5,
+                        maxLength:10,
+                      },
+                    ]
+            },
+  repeatpassword:{  
+              rules:[
+                      {
+                        name:'onlyNumberAndLetterSimbols',
+                        message:'Numero y letras. Simbolos permitidos [@,.,-,_,#]'
+                      },
+                      {
+                        name:'compareField',
+                        matchField:'password',
+                        message:'No Coincide la contrasena.'
+                      },
+                      {
+                        name:'minMaxLength',
+                        message:'Minimo de caracteres 5 y maximo 10.',
+                        minLength:5,
+                        maxLength:10,
+                      },
+                    ],
+                    ignoreFieldForsave:true,
+                },
+  company:{  
+          rules:[
+                  {
+                    name:'onlyNumberAndLetterSimbolsSpaces',
+                    message:'Numero y letras. Simbolos permitidos [@,.,-,_,#]'
+                  },
+                ]
+          },
+  company_ssid:{  
+                  rules:[
+                          {
+                            name:'onlyNumber',
+                            message:'Solo se Permiten Numeros'
+                          },
+                        ]
+                },
+  role:{  
+          rules:[
+                  {
+                    name:'onlyLetters',
+                    message:'Seleccione el valor correcto.'
+                  },
+                ]
+        },
+  firstname:{  
+              rules:[
+                      {
+                        name:'onlyLettersSpaces',
+                        message:'Numero y letras. Simbolos permitidos [@,.,-,_,#]'
+                      },
+                    ]
+            },
+  lastname:{  
+              rules:[
+                      {
+                        name:'onlyLettersSpaces',
+                        message:'Numero y letras. Simbolos permitidos [@,.,-,_,#]'
+                      },
+                    ]
+            },
+  email:{  
+              rules:[
+                      {
+                        name:'email',
+                        message:'ingrese un correo electronico valido'
+                      },
+                      {
+                        name:'isUnique',
+                        message:'Correo Electronico ya registrado.'
+                      },
+                    ]
+        },
+}
+export const listRex = async ({field,rule,value,matchField,matchFieldvalue,minLength=0,maxLength=0})=>{
+ let Regex = null;
+ let checkSpecial = false;
+ let result = false;
+  switch (rule) {
+  case 'onlyNumberAndLetter': Regex = /^[a-zA-Z0-9]*$/;break;
+  case 'onlyNumberAndLetterSimbols': Regex = /^[a-zA-Z0-9\_.-@*#]*$/;break;
+  case 'onlyNumber': Regex = /^[0-9]*$/;break;
+  case 'onlyLetters': Regex = /^[a-zA-Z]*$/;break;
+  case 'onlyLettersSpaces': Regex = /^[a-zA-Z\s]*$/;break;
+  case 'onlyNumberAndLetterSimbolsSpaces': Regex = /^[a-zA-Z0-9\_.-@*#\s]*$/;break;
+  case 'email': Regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;break;
+  case 'isUnique': checkSpecial=true;  Regex = await checkIsUnique({field,value}); result=!!Regex.isunique; break;
+  case 'compareField': checkSpecial=true;  result = value === matchFieldvalue ? true : false; break;
+  case 'minLength': checkSpecial=true; result = value.length >= minLength ? true : false;  break;
+  case 'maxLength': checkSpecial=true; result = value.length <= maxLength ? true : false;  break;
+  case 'minMaxLength': checkSpecial=true; result = value.length >= minLength && value.length <= maxLength  ? true : false;  break;
 
+
+ }
+if(checkSpecial){
+  return result;
+}else{
+ const regex_test = new RegExp(Regex,"g");
+ return regex_test.test(value);
+}
+}
+export const startUp = async ({data,schema})=>{
+  let error={};
+  const dataUserObj = {};
+    for (const value of Object.entries(schema)) {
+      for (const valueChildlren of value[1].rules) {
+        const keyName=value[0];
+        const ruleName = valueChildlren.name; 
+        const valueField = data[value[0]];
+        const matchField = valueChildlren.matchField || undefined;
+        const matchFieldvalue = data[matchField] || undefined;
+        const minLength = valueChildlren.minLength || undefined;
+        const maxLength = valueChildlren.maxLength || undefined;
+        const result = await listRex({field:keyName,rule:ruleName,value:valueField,matchField,matchFieldvalue,minLength,maxLength});
+        if(!result){
+          error[keyName]=valueChildlren.message;
+        }else if(!valueChildlren.ignoreFieldForsave){
+          dataUserObj[keyName]=valueField;
+        }
+      }
+  }
+  return {error,dataUserObj};
+}
+export async function checkIsUnique({field,value,infoUser=false}){
+  const query = {[field]:value};
+  const dataResponse = await fetch(`${URLDEV}/api/users/checkisunique`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(query)
+  }).then(response =>response.json()).then((data)=>{
+    return {...data};
+  });
+return dataResponse;
+
+}
 export async function getlogin({username,password}){
     const dataUpdate = {username,password};
 
-    const dataResponse = await fetch('https://manager-backend.lvelarde01.repl.co/api/users/login', {
+    const dataResponse = await fetch(`${URLDEV}/api/users/login`, {
         method: 'POST',
         headers: {
           'Accept': 'application/json',
@@ -20,11 +190,23 @@ export async function getlogin({username,password}){
       });
     return dataResponse;
 }
+export async function newUser(dataUserObj){
+    const dataResponse = await fetch(`${URLDEV}/api/users/add`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataUserObj)
+      }).then(response =>response.json());
+    return dataResponse;
+
+}
 export async function getinfoUser(){
     const {id,token} =  await localforage.getItem('user');
     const dataUpdate = {_id:id,token};
 
-    const dataResponse = await fetch('https://manager-backend.lvelarde01.repl.co/api/users/info', {
+    const dataResponse = await fetch(`${URLDEV}/api/users/info`, {
         method: 'POST',
         headers: {
           'Accept': 'application/json',
@@ -38,7 +220,7 @@ export async function updateinfoUser(dataUserObj){
     const {id,token} =  await localforage.getItem('user');
     const dataUpdate = {query:{_id:id,token},data:{...dataUserObj}};
 
-    const dataResponse = await fetch('https://manager-backend.lvelarde01.repl.co/api/users/updateinfo', {
+    const dataResponse = await fetch(`${URLDEV}/api/users/updateinfo`, {
         method: 'POST',
         headers: {
           'Accept': 'application/json',
@@ -76,10 +258,22 @@ export async function checkLogin(){
     console.log("Check Login user.js");
     return dataUserObj;
 }
+export async function getBase64(file, onLoadCallback) {
+  return await new Promise(function(resolve, reject) {
+      var reader = new FileReader();
+      reader.onload = function() { resolve(reader.result); };
+      reader.onerror = reject;
+      reader.readAsDataURL(file);
+  });
+}
 export async function clearUser(){
+    const dataUserObj =  await localforage.getItem('user'); 
+    if(dataUserObj?.saveConfigBrowser === true ){
+      return await setLocal({theme:dataUserObj.theme,token:false});
+    }
     return await localforage.removeItem('user');
 }
-function setLocal(dataUserObj){
-  return  localforage.setItem('user', dataUserObj);
+export async function setLocal(dataUserObj){
+  return await localforage.setItem('user', dataUserObj);
 
 }
