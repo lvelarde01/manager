5c1826494b0525eb77b63420332db0c409e160ae
Luis Velarde <velardeluisangel@gmail.com>
2022-12-16 09:54:42 -0400

    [Update] Fixes of routes.

--- src/main.js
+++ src/main.js
@@ -5,17 +5,18 @@ import { ThemeProvider } from './context/theme-context'
 import { checkLogin } from './requests/users'
 import { redirect } from 'react-router-dom'
 export async function loader({ request }){
-  const {token} = await checkLogin();
+  const userInfo = await checkLogin();
   const url = new URL(request.url);
   const pathname = url.pathname;
   
-  if(!token && pathname !== "/login"){
+  if(!userInfo.token && pathname === "/register"){
+    return {work:"yes"};
+  }else if(!userInfo.token && pathname !== "/login"){
     return redirect("/login");
-  }
-  if(token && pathname === "/login"){
+  }else if(userInfo.token && pathname === "/login"){
     return redirect("/");
   }
-  return {pathname,token};
+  return {pathname,userInfo};
 } 
 export default function Main() {
   return (
