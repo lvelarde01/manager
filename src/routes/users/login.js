019498ec144639421d16670df7bcae5a08d49c83
Luis Velarde <velardeluisangel@gmail.com>
2023-02-13 14:24:19 -0400

    [Update] Add OAuth Google

--- src/routes/users/login.js
+++ src/routes/users/login.js
@@ -1,16 +1,44 @@
-import React, { useContext,useState} from 'react'
+import React, { useContext,useState,useEffect} from 'react'
 import {Link,Form,useNavigate} from 'react-router-dom';
-import { getlogin,startUp,schema_login } from '../../requests/users';
+import { getlogin,getloginAuthGoogle,startUp,schema_login } from '../../requests/users';
 import AlertMessage from '../../assets/alertmessage';
 import AuthContext from "../../context/auth-context";
+import ThemeContext from '../../context/theme-context';
 import FooterCustom from '../../assets/FooterCustom';
-
+import Recovery from './recovery';
+import jwt_decode from 'jwt-decode';
+import NavbarCustom from '../../assets/NavbarCustom';
 export async function loader({ request}) {
 
 }
 
 export default function Login() {
+  
+  async function hanlderLogin(response){
+      const result = await getloginAuthGoogle(response);
+       await handlerAuth(result);
+      navigate('/');
+    
+    console.log(response);
+    let dataUserObj = jwt_decode(response.credential);
+    console.log(dataUserObj);
+  }
+  
+  useEffect(()=>{
+    window.google.accounts.id.initialize({
+      client_id:process.env.REACT_APP_GOOGLEAPI,
+      callback: hanlderLogin,
+    });
+    window.google.accounts.id.renderButton(
+      document.getElementById('btnoAuth'),
+      { type: "standard", theme: "filled_blue", size: "large", shape: "rectangular", width: "1024", logo_alignment: "right" },
+    );
+  },[]);
+
+
   const {Auth,handlerAuth} = useContext(AuthContext);
+  const {theme,handlerTheme} = useContext(ThemeContext);
+
   const navigate = useNavigate();
   const [fetchReady,setFetchReady] = useState({ready:false,msgtype:'success',message:'default'});
   const [errors,setErrors] = useState({});
@@ -60,34 +88,25 @@ export default function Login() {
   
 
   return (
-    <div className={`vw-100 ${Auth.theme || "green" }-style-login`}>
+    <div className={`vw-100 ${Auth.theme || theme}-style-login background-default`}>
+      <NavbarCustom />
+      <Recovery />
   <div className="container-fluid h-custom ">
-
+  
     <div className="row d-flex justify-content-center align-items-center h-100">
       <div className="col-md-9 col-lg-6 col-xl-5">
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-          className="img-fluid" alt='Imagen Ilustrativa'/>
+        <img src={'/img/delivery3.png'} className="img-fluid" alt='Imagen Ilustrativa'/>
       </div>
-      <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-        {fetchReady.ready && (<AlertMessage sizeClass={"col-12"} message={fetchReady.message} msgtype={fetchReady.msgtype} />) }
+      <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1 block-radius-style p-5">
+        {fetchReady.ready && (<AlertMessage sizeClass={"col-12"} message={fetchReady.message} msgtype={fetchReady.msgtype} typeAlert={"custom"} />) }
         <Form method='post' onSubmit={handlerOnSubmit}>
-          <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-            <p className="lead fw-normal mb-0 me-3">Acceder con </p>
-            <button type="button" className="btn btn-primary btn-floating rounded-circle mx-1">
-              <i className="fab fa-facebook-f"></i>
-            </button>
-
-            <button type="button" className="btn btn-primary btn-floating mx-1 rounded-circle ">
-              <i className="fab fa-twitter"></i>
-            </button>
-
-            <button type="button" className="btn btn-primary btn-floating mx-1 rounded-circle">
-              <i className="fab fa-linkedin-in"></i>
-            </button>
+          <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-center">
+            
+          <div className='ms-2 mt-2' id='btnoAuth'></div>
           </div>
-
+          
           <div className="divider d-flex align-items-center my-4">
-            <p className="text-center fw-bold mx-3 mb-0">O</p>
+            <p className="text-center fw-bold mx-3 mb-0">Iniciar Sesion</p>
           </div>
 
           
@@ -113,17 +132,18 @@ export default function Login() {
                 Recordar
               </label>
             </div>
-            <Link to={"/"} className="text-body">Olvido su contrasena?</Link>
+            <Link to={"/"} data-bs-toggle='modal' data-bs-target='#recoveryModal' className="text-body">Olvido su contrasena?</Link>
           </div>
 
           <div className="text-center text-lg-start mt-4 pt-2">
             <button type="submit" className="btn btn-primary btn-lg" disabled={loading} >
             {loading ? <><span className="spinner-grow spinner-grow-sm me-2"></span><span>Iniciando</span></>  : <><i className="fas fa-key me-2"></i>Iniciar</> }
             </button>
-            <p className="small fw-bold mt-2 pt-1 mb-0">No tienes una Cuenta ? crear una cuenta <Link to={"/register"}
-                className="link-danger">Registar</Link></p>
+             <Link to={"/register"}
+                className=" btn btn-primary btn-lg float-end"><i className="fas fa-user me-2"></i>Registar</Link>
+                
           </div>
-
+          
         </Form>
       </div>
     </div>
